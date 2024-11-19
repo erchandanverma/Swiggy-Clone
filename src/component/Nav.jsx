7cc963922542426085ad4fea1b4ec9c19067c43f
Chandan Verma <154034889+erchandanverma@users.noreply.github.com>
2024-11-19 16:30:14 +0530

    Update Nav.jsx

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,51 +1,29 @@
 import React from 'react';
-import Logo from '../assets/logo.png'; // Ensure the logo path is correct
-import './Nav.css'; // Ensure styles are defined here
+import Logo from '../assets/logo.png'; 
+import './Nav.css'; 
 
 const Nav = () => {
   const NavData = [
-    {
-      id: 1,
-      name: 'Swiggy Corporate',
-      icon: 'fas fa-building',
-    },
-    {
-      id: 2,
-      name: 'Search',
-      icon: 'fas fa-search',
-    },
-    {
-      id: 3,
-      name: 'Offer',
-      icon: 'fas fa-gift',
-    },
-    {
-      id: 4,
-      name: 'Help',
-      icon: 'fas fa-question-circle',
-    },
-    {
-      id: 5,
-      name: 'Signin',
-      icon: 'fas fa-sign-in-alt',
-    },
-    {
-      id: 6,
-      name: 'Cart',
-      icon: 'fas fa-shopping-cart',
-    },
+    { id: 1, name: 'Swiggy Corporate', icon: 'fas fa-building' },
+    { id: 2, name: 'Search', icon: 'fas fa-search' },
+    { id: 3, name: 'Offer', icon: 'fas fa-gift' },
+    { id: 4, name: 'Help', icon: 'fas fa-question-circle' },
+    { id: 5, name: 'Signin', icon: 'fas fa-sign-in-alt' },
+    { id: 6, name: 'Cart', icon: 'fas fa-shopping-cart' },
   ];
 
   return (
-    <nav className="navbar container-fluid p-5">
-      <div className="navbar-left">
-        <img src={Logo} alt="Logo" className="navbar-logo" />
-        <p style={{textDecoration:"underline"}}>Other <i className="fa-solid fa-angle-down" style={{color:"orange"}}></i></p>
+    <nav className="navbar container-fluid p-5 row">
+      <div className="navbar-left col-lg-6 col-md-12 d-flex align-items-center">
+        <img src={Logo} alt="Swiggy Logo" className="navbar-logo mx-2" />
+        <p className="dropdown-indicator">
+          Other <i className="fa-solid fa-angle-down" style={{ color: "orange" }}></i>
+        </p>
       </div>
-      <div className="navbar-right">
-        <ul className="navbar-links">
+      <div className="navbar-right col-lg-6 col-md-12">
+        <ul className="navbar-links d-flex list-unstyled">
           {NavData.map((item) => (
-            <li key={item.id} className="nav-item">
+            <li key={item.id} className="nav-item mx-2">
               <a href="#" className="nav-link">
                 <i className={item.icon}></i> <span>{item.name}</span>
               </a>
